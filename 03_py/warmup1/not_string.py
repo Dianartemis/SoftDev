57b14829ccb9368a1fc9417bca59e6609205fdf5
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-09-22 13:21:13 -0400

    ack

--- 03_py/warmup1/not_string.py
+++
@@ -1,9 +0,0 @@
-def not_string(str):
-  if(str[:3] == "not"):
-    return str
-  else: 
-    return "not " + str
-
-print(not_string('candy'))
-print(not_string('x'))
-print(not_string('not bad'))
--- 03_py/warmup2/array123.py
+++
@@ -1,7 +0,0 @@
-def array123(nums):
-  return (1 in nums) and (2 in nums) and (3 in nums)
-
-
-print(array123([1, 1, 2, 3, 1]))
-print(array123([1, 1, 2, 4, 1]))
-print(array123([1, 1, 2, 1, 2, 3]))
