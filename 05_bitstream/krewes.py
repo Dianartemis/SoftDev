5e9922f0e256f0de8c33a007a976ef72ea496989
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-09-28 13:22:07 -0400

    05_bitstream

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,23 @@
+""" 
+Russel Goychayev, Diana Akhmedova, Kevin Liu
+SoftDev
+05_bitstream
+2022-09-28
+time spent:  hours
+
+DISCO: 
+ - 
+
+QCC:
+ - 
+
+OPS SUMMARY:
+ 1. 
+"""
+
+txt = open("krewes.txt")
+text = txt.read()
+
+# print(text)
+
+text.split()
