620940e17a8e67e7ef770eb2b260cfe0fe2b4fd2
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-10-07 13:11:33 -0400

    finish readme

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -9,7 +9,7 @@ time spent: 0.5 hours
 from flask import Flask
 app = Flask(__name__) # What do the underscores between name mean?
 
-@app.route("/") # This part of the code routes the return of hello_world() onto a web page.\
+@app.route("/") # This part of the code routes the return of hello_world() onto a web page.
                 # The "/" stands for the root of the directory.
 def hello_world():
     print(__name__) # print() will print "__main__" in the terminal.
