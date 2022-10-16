fc598f566330cb205e4232dd08e1e3c354415d63
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2022-10-16 17:28:46 -0400

    finish k11

--- 11_flask-forms/app.py
+++ 11_flask-forms/app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)    #create Flask object
 trioTASK:
 ~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
 ...read for understanding all of the code below.
-Some will work as written; other sections will not. 
+Some will work as written; other sections will not.
 TASK: Predict which...
 Devise some simple tests you can run to "take apart this engine," as it were.
 Execute your tests.
@@ -30,7 +30,7 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -39,14 +39,14 @@ def disp_loginpage():
     print(request) # We believe that this will not work because it doesn't request the argument before printing; might return memory address.
     print("***DIAG: request.args ***")
     print(request.args) # args is from Flask.
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***")
+    print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers) # headers is from HTML.
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -55,15 +55,15 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print("***DIAG: request.args['username']  ***")
+    print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
     return "Waaaa hooo HAAAH"  #response to a form submission
 
 
-    
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
--- 11_flask-forms/readme.md
+++ 11_flask-forms/readme.md
@@ -6,18 +6,19 @@
 <br>
 
 # DISCO:
-Predictions:
+## Predictions:
 * We believe that this will not work because it doesn't request the argument before printing; might return memory address.
 * args is from Flask.
 * headers is from HTML.
 * The webserver will return "Waaaa hooo HAAAH" when a form is submitted.
 
-Observations:
+## Observations:
 * A request object is the location of the Flask app.
     * ex: http://127.0.0.1:5000/auth?username=ADJ+In+The+House%21&sub1=Submit+Query
 * The request argument is a ImmutableMultiDict([]), which stores the username and the submit button once inputted.
     * The username and sub1 are keys named from the HTML files.
     * The user inputs and "Submit Query" are the values.
+    * request.args outputs the user's input for the username after the user submits it.
 * The request headers include:
     * Host: 127.0.0.1:5000
     * User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0
@@ -38,3 +39,6 @@ Observations:
 
 # QCC:
 * Where did the "Submit Query" name come from that is displayed in the terminal and on the webserver?
+* Are there any other type of methods besides GET and POST?
+* Why does running the webserver still work without the GET and POST methods?
+* What does "Upgrade-Insecure-Requests: 1" mean?
