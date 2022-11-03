5fb97361f9524977abb05a68847efe86fc4ae033
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-11-03 13:13:38 -0400

    start k19

---
+++ 19_session/app.py
@@ -0,0 +1,13 @@
+# DEM PUMPKINS: Emily Ortiz, Diana Akhmedova, May Qiu
+# SoftDev
+# K19 -- Sessions Greetings
+# 2022-11-03
+# time spent:  hours
+
+from flask import session
+import os
+
+app.secret_key = os.urandom(32)
+
+@app.route('/')
+
