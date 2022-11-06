20589a774f19f517b3685acdc375afa118e0fee8
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2022-11-06 00:37:25 -0400

    finish k19

--- 19_session/app.py
+++ 19_session/app.py
@@ -2,60 +2,75 @@
 # SoftDev
 # K19 -- Sessions Greetings
 # 2022-11-03
-# time spent:  hours
+# time spent: 3.5 hours
 
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from flask import redirect
 from flask import session
 import os
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
+# not sure what this does
+# code doesn't work without it
 app.secret_key = os.urandom(32)
+
 expected_user = "Elmo"
 expected_pass = "1234"
 
+user = "" # global var, meant to be inputed username later
+
 @app.route("/")
 def index():
-    if 'username' in session: # if cookie is already in session
-        response()
-    return login()
-    # return render_template( 'login.html' ) 
+    # if user is already in session
+    if 'username' in session:
+        print("user is in session")
+        return redirect("/welcome") # redirects to welcome page, user is already logged in
+    # the instant user clicks submit, method is POST
+    elif request.method == 'POST':
+        print("user is NOT in session")
+        return redirect("/login") # redirects to login, which checks if user/pass are correct
+    # method is GET
+    return render_template('login.html') # displays login page w/out error_msg
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST': # user is logged in
-        session['username'] = request.form['username'] # create a cookie w/ username
-        return render_template( 'response.html' )
-    
-    
+    username = request.form.get('username') # username user inputs on form
+    password = request.form.get('password') # password user inputs on form
+
+    user = username # setting global var to inputed username
+    print(user)
+
+    # if username and password are correct
+    if (username == expected_user) and (password == expected_pass):
+        session['username'] = request.form['username'], request.form['password'] # create a session/cookie w/username+password
+        print("session started")
+        return redirect("/welcome") # redirects to welcome, user is logged in and in session
+    # if password is wrong
+    elif (username == expected_user):
+        return render_template( 'login.html', error_msg="Password is incorrect.") # displays login page w/error_msg
+    # if username is wrong
+    elif (password == expected_pass):
+        return render_template( 'login.html', error_msg="Username is incorrect.") # displays login page w/error_msg
+    # both username and password are wrong
+    return render_template('login.html', error_msg="Username and Password are incorrect.") # displays login page w/error_msg
+
 
 @app.route("/welcome", methods=['GET', 'POST'])
 def response():
-    return render_template( 'response.html', username=request.form.get('username'), method=request.method )
+    return render_template( 'response.html', response_username="Elmo") # displays response page with the user inputted username
 
 
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    session.pop('username') # removes user from session
+    print("user is NOT in session")
+    return redirect("/") # redirects to initial page
+
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
-
-
-# def index():
-#     if 'username' in session:
-#         return f'Logged in as {session["username"]}'
-#     return 'You are not logged in'
-
-
-# if cookie alr :
-#     go to welcome page
-# if no cookie:
-#     have user fill out login form
-#     if login correct:
-#         create cookie
-#         go to welcome page
-#     else (login wrong):
-#         tell user that
