f22db79983672cd758b593b4fc595b454c0d3956
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-04-04 13:19:18 -0400

    k27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,13 @@
+// Team Dark Lord Chuckles the Silly Pig :: Lauren Lee, Thomas Zhang, Diana Akhmedova 
+// SoftDev pd7
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
