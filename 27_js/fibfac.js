f7f6778448d88c2d7f9e29c2b1f2014c3c95338e
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-04-04 20:02:25 -0400

    finish k27

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -9,6 +9,8 @@
 // pair programming style,
 // implement a fact and fib fxn
 
+
+// FORMULA: fact(n) = n * fact(n - 1) OR n! = n * (n - 1)!
 function fact(n) {
     if (n < 2) {
         return 1;
@@ -20,10 +22,12 @@ console.log(fact(1));
 console.log(fact(3));
 console.log(fact(5));
 
+
+// FORMULA: fib(n) = fib(n - 1) + f(n - 2)
 function fib(n) {
-    if (n = 0) {
+    if (n == 0) {
         return 0;
-    } if (n = 1) {
+    } if (n == 1) {
         return 1;
     } return (fib(n - 1) + fib(n - 2));
 }
@@ -32,5 +36,6 @@ console.log("\nRETURNING FIB...");
 console.log(fib(0));
 console.log(fib(1));
 console.log(fib(5));
+
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
