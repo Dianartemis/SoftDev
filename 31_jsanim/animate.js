b92b46ed131db648bd0e6ab1d16a29826bfbda5d
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-04-25 22:54:21 -0400

    finish k31

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -8,7 +8,7 @@ ctx.fillStyle = "blue";
 
 var requestID; // init global var for use with animation frames
 
-var clear = () => { // why does it need parameter e ????
+var clear = (e) => {
     ctx.clearRect(0, 0, c.width, c.height);
 };
 
@@ -17,26 +17,34 @@ var growing = true;
 
 var drawDot = () => {
     clear();
-    window.requestAnimationFrame();
+    window.cancelAnimationFrame(requestID);
+    requestID = window.requestAnimationFrame(drawDot);
     ctx.beginPath();
     ctx.arc(c.width / 2, c.height / 2, radius, 0, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
-    
-    // wipe the canvas, repaint circle, increment radius, if circle hit canvas border, decrement radius
-    // window.cancelAnimationFrame()
-    // window.requestAnimationFrame()
+    if (growing == true) {
+        if (radius > c.width / 2) {
+            growing = false;
+        }
+        radius ++;
+        console.log(radius)
+    }
+    if (growing == false) {
+        if (radius == 1) {
+            growing = true;
+        }
+        radius --;
+        console.log(radius)
+    }
 }
 
-// //var stopIt = function() {
-// var stopIt = () => {
-//     console.log("stopIt invoked...")
-//     console(requestID);
-
-//     if (growing == true) {
-//         window.cancelAnimationFrame();
-//     }
-// }
+//var stopIt = function() {
+var stopIt = () => {
+    console.log("stopIt invoked...");
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
+}
 
 dotButton.addEventListener("click", drawDot);
-// stopButton.addEventListener("click", stopIt);
+stopButton.addEventListener("click", stopIt);
